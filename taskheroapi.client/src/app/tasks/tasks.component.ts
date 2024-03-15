1c45640bd08839e1abcbc3df85658ce6d74ab141
Aaliyah Loechner <aaliyahloechner@gmail.com>
2024-03-14 21:40:25 -0400

    commit for merge conflicts

--- taskheroapi.client/src/app/tasks/tasks.component.ts
+++ taskheroapi.client/src/app/tasks/tasks.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Task } from '../model/task';
 import { CrudService } from '../service/crud.service';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { userTask } from '../../services/userTasks.service';
 import { IUserTasks } from '../../interfaces/usertasks.interface';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, OnDestroy {
 
   tasks: IUserTasks[] = [];
   currentUser: number = 0;
@@ -27,12 +28,13 @@ export class TasksComponent implements OnInit {
   taskArr: Task[] = [];
   addTaskValue: string = '';
   editTaskValue: string = '';
+  taskSubscription: Subscription | undefined;
 
   constructor(
+    private crudService: CrudService,
     private authService: AuthService,
-    private TaskService: userTask,
     private router: Router,
-    private crudService: CrudService
+    private TaskService: userTask
   ) { }
 
   ngOnInit(): void {
@@ -47,61 +49,72 @@ export class TasksComponent implements OnInit {
   }
 
   getAllTask() {
-    this.crudService.getAllTask().subscribe(
-      (res: Task[]) => {
+    this.taskSubscription = this.crudService.getAllTask().subscribe({
+      next: (res: Task[]) => {
         this.taskArr = res;
       },
-      (err) => {
+      error: (err) => {
         console.error("Unable to get list of tasks", err);
       }
-    );
+    });
   }
 
   addTask() {
     console.log('adding...')
     this.taskObj.task_name = this.addTaskValue;
-    this.crudService.addTask(this.taskObj).subscribe(
-      (res) => {
-        this.ngOnInit(); 
+    this.crudService.addTask(this.taskObj).subscribe({
+      next: (res) => {
+        this.ngOnInit();
         this.addTaskValue = '';
       },
-      (err) => {
+      error: (err) => {
         console.error("Failed to add task", err);
       }
-    );
+    });
   }
 
   editTask() {
     this.taskObj.task_name = this.editTaskValue;
-    this.crudService.editTask(this.taskObj).subscribe(
-      (res) => {
-        this.ngOnInit(); 
+    this.crudService.editTask(this.taskObj).subscribe({
+      next: (res) => {
+        this.ngOnInit();
       },
-      (err) => {
+      error: (err) => {
         console.error("Failed to update task", err);
       }
-    );
+    });
   }
 
   deleteTask(etask: Task) {
-    this.crudService.deleteTask(etask).subscribe(
-      (res) => {
+    this.crudService.deleteTask(etask).subscribe({
+      next: (res) => {
         this.ngOnInit();
       },
-      (err) => {
+      error: (err) => {
         console.error("Failed to delete task", err);
       }
-    );
+    });
   }
 
   getUserTasks() {
-    this.TaskService.getUserTasks(this.currentUser).subscribe(tasks => {
-      this.tasks = tasks;
+    this.TaskService.getUserTasks(this.currentUser).subscribe({
+      next: (tasks: IUserTasks[]) => {
+        this.tasks = tasks;
+      },
+      error: (err) => {
+        console.error("Failed to get user tasks", err);
+      }
     });
   }
 
   call(etask: Task) {
     this.taskObj = etask;
     this.editTaskValue = etask.task_name;
+  }
+
+  ngOnDestroy() {
+    if (this.taskSubscription) {
+      this.taskSubscription.unsubscribe();
+    }
   }
 }
