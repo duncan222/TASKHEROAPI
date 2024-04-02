5d65cb4466b709c5307547fb243d372f69dfc983
Aaliyah Loechner <aaliyahloechner@gmail.com>
2024-04-01 22:42:05 -0400

    initial commit for tasks component

--- taskheroapi.client/src/app/tasks/tasks.component.ts
+++ taskheroapi.client/src/app/tasks/tasks.component.ts
@@ -1,61 +1,163 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { userTask } from '../../services/userTasks.service';
-import { IUserTasks } from '../../interfaces/usertasks.interface';
 import { AuthService } from '../../services/auth.service';
+import { IUserTasks } from '../../interfaces/usertasks.interface';
+import { userAchievements } from '../../services/userAchievement.service';
+import { IUserAchievements } from '../../interfaces/userachievements.interface';
+import { Achievements } from '../../services/achievements.service';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
-
-  constructor(private authService: AuthService,private TaskService: userTask, private router: Router) { }
-
-  //array for all the users tasks 
-  tasks: IUserTasks[] = [];
-
-  //the different variables to make up the task interface. 
+export class TasksComponent implements OnInit {
   currentUser: number = 0;
-  description: string = "";
-  timeStamp: string = "";
-  title: string = "";
-  dueDate: string = "";
-  importance: number = 0;
-  weight: number = 0;
-  urgency: number = 0;
+  taskGroup: FormGroup;
+  tasks: IUserTasks[] = []; // Array to store tasks
+  user_achievements: any;
+  showNotification = false;
+  notificationMessage = "";
+  color = "";
+  weeklytasks = 0;
 
-  addTask() { 
-    const tempTask: IUserTasks = { 
-      TaskId: 0, 
-      UserId: 0, 
-      Description: this.description, 
-      TimeStamp: this.timeStamp, 
-      Title: this.title, 
-      DueDate: this.dueDate, 
-      Importance: this.importance, 
-      Weight: this.weight, 
-      Urgency: this.urgency
-    }
-
-    this.TaskService.addTask(this.currentUser, tempTask)
-      .subscribe((addedTask: IUserTasks) => { 
-        console.log('Task added: ', addedTask); 
-        this.getUserTasks(); 
-      })
+  constructor(
+    private fb: FormBuilder,
+    private taskService: userTask,
+    private authService: AuthService,
+    private Achievements: userAchievements,
+    private achievements: Achievements
+  ) {
+    this.taskGroup = this.fb.group({
+      title: new FormControl(''),
+      description: new FormControl(''),
+      date: new FormControl(new Date()),
+      priority: new FormControl('')
+    });
   }
 
-  getUserTasks(){ 
-    this.TaskService.getUserTasks(this.currentUser)
-      .subscribe(tasks => { 
-        this.tasks = tasks; 
-      })
-  }
+  startDate = new Date();
+  minDate = new Date();
 
-  ngOnInit() { 
-    if(this.authService.getLoggedInUserId != null){
+  ngOnInit(): void {
+    if (this.authService.getLoggedInUserId != null) {
       this.currentUser = Number(this.authService.getLoggedInUserId());
     }
+    this.getAchievements();
+  }
 
+  onSubmit(): void {
+    if (this.taskGroup.valid && this.taskGroup != null) {
+      const descriptionControl = this.taskGroup.get('description');
+      const dueControl = this.taskGroup.get('date');
+      const titleControl = this.taskGroup.get('title');
+      const priorityControl = this.taskGroup.get('priority');
+
+      if (descriptionControl && dueControl && titleControl && priorityControl) {
+        const urgency = this.Urgency(new Date(), dueControl.value);
+        const weight = this.Weight(urgency, priorityControl.value);
+        const taskInstance: IUserTasks = {
+          TaskId: 0,
+          UserId: this.currentUser,
+          Description: descriptionControl.value,
+          TimeStamp: new Date().toString(),
+          Title: titleControl.value,
+          DueDate: dueControl.value.toString(),
+          Importance: priorityControl.value,
+          Weight: weight,
+          Urgency: urgency,
+        };
+
+        // Push the submitted task to the tasks array
+        this.tasks.push(taskInstance);
+        // Clear the form after submission
+        this.taskGroup.reset();
+
+        console.log(taskInstance);
+        this.taskService.addTask(this.currentUser, taskInstance).subscribe(
+          response => {
+            console.log("added")
+            this.notificationMessage = "Task Added!"
+            this.color = "#198754";
+            this.showNotification = true;
+            setTimeout(() => {
+              this.showNotification = false;
+            }, 5000);
+          },
+          error => {
+            console.error('Error saving todo:', error);
+          }
+        )
+      }
+    }
+  }
+  updateAchievements(currentUser: number, acheivement: IUserAchievements): void {
+    this.Achievements.update(currentUser, acheivement)
+      .subscribe({
+        next: () => {
+          //successfully updates values of the users ahcievements 
+          this.getAchievements();
+        }
+        ,
+        error: error => {
+          console.log('error updating', error);
+        }
+      });
+
+  }
+
+  calculateSunday(due: string): boolean {
+    // getting the date value of 'next sunday'
+    const today = new Date();
+    const daysOfWeek = today.getDay();
+    const daysUntilSunday = daysOfWeek === 0 ? 7 : 7 - daysOfWeek;
+    const nextSunday = new Date(today);
+    nextSunday.setDate(today.getDate() + daysUntilSunday);
+    nextSunday.setHours(0, 0, 0, 0);
+    //returning the length of the filtered tasks... all that for that. has to be easier way 
+    return new Date(due) < nextSunday;
+  }
+
+  hasSundaySinceDate(startDate: Date): boolean {
+    const currentDate = new Date();
+    let currentDateToCheck = new Date(startDate);
+    while (currentDateToCheck <= currentDate) {
+      if (currentDateToCheck.getDay() === 0) {
+        return true;
+      }
+      currentDateToCheck.setDate(currentDateToCheck.getDate() + 1);
+    }
+    return false;
+  }
+
+  getAchievements() {
+    this.Achievements.getAchievements(this.currentUser)
+      .subscribe({
+        next: (achievements) => {
+          this.user_achievements = achievements;
+        },
+        error: (error) => {
+          console.error('An error occurred:', error);
+        },
+        complete: () => { }
+      });
+  }
+
+  Urgency(date1: Date, date2: Date): number {
+    const mSecPerDay = 1000 * 60 * 60 * 24;
+    const timeDiffMs = date2.getTime() - date1.getTime();
+    const timeDiffFromNow = date2.getTime() - (new Date()).getTime();
+    const urgency = 1 - ((timeDiffFromNow / mSecPerDay) / (timeDiffMs / mSecPerDay));
+    return urgency;
+  }
+
+  Weight(urgency: number, priority: number): number {
+    const UrgencyWeight = .8;
+    const PriorityWeight = .2;
+    const NormalizePriority = Math.max(0, Math.min(1, priority));
+    const NormalizeUrgency = Math.max(0, Math.min(1, urgency));
+    const weight = (UrgencyWeight * NormalizeUrgency) + (PriorityWeight * NormalizePriority);
+    return weight;
   }
 }
