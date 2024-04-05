f9edbecc891ae64306fdeb45b86647992e7a888c
Brad Didier <braddidier13244@gmail.com>
2024-04-04 22:35:25 -0400

    I'm confused

--- taskheroapi.client/src/app/tasks/tasks.component.ts
+++ taskheroapi.client/src/app/tasks/tasks.component.ts
@@ -10,6 +10,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { EditTaskComponent } from '../edit-task/edit-task.component';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 import { DOCUMENT } from "@angular/common";
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-tasks',
@@ -26,6 +27,12 @@ export class TasksComponent implements OnInit {
   notificationMessage = "";
   color = "";
   weeklytasks = 0;
+  comicChoice: string = "";
+  comicExpressions: string[] = ['/assets/icons/kaboom.png', '/assets/icons/boom.png', '/assets/icons/pow.png', '/assets/icons/kapow.png', '/assets/icons/bang.png'];
+  showImagePop = false;
+  ProgressCount: number = 0;
+  totalScore: number = 0;
+  userdetails: any = "";
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
@@ -34,7 +41,8 @@ export class TasksComponent implements OnInit {
     private authService: AuthService,
     private Achievements: userAchievements,
     private achievements: Achievements,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private userService: UserService
   ) {
     this.taskGroup = this.fb.group({
       title: new FormControl(''),
@@ -51,6 +59,16 @@ export class TasksComponent implements OnInit {
     if (this.authService.getLoggedInUserId != null) {
       this.currentUser = Number(this.authService.getLoggedInUserId());
     }
+    this.userService.getUserById(this.currentUser).subscribe({
+      next: (userDetails) => {
+        this.userdetails = userDetails;
+      }
+      ,
+      complete: () => {
+        console.log(this.userdetails);
+      }
+    }
+    );
     this.getAchievements();
     this.loadTasks();
     // Subscribe to the observable that emits the updated task list
@@ -107,6 +125,14 @@ export class TasksComponent implements OnInit {
           Urgency: urgency,
         };
 
+        var locked_and_unlocked = this.achievements.determineAcheivements(
+          this.user_achievements.unlockedAchievements,
+          this.user_achievements.lockedAchievements,
+          this.user_achievements.dailyTracker,
+          "add task",
+          this.user_achievements.tasksCompleted
+        );
+
         // Add the submitted task to the tasks array
         this.tasks.push(taskInstance);
         // Clear the form after submission
@@ -114,12 +140,34 @@ export class TasksComponent implements OnInit {
 
         console.log(taskInstance);
 
+        var weeklytask = 0; 
+
         this.taskService.addTask(this.currentUser, taskInstance).subscribe(
           response => {
             console.log("added")
             this.notificationMessage = "Task Added!"
             this.color = "#198754";
             this.showNotification = true;
+            if (this.calculateSunday(taskInstance.DueDate)) {
+              console.log("post")
+              weeklytask = 1;
+            }
+            var AchievemtUpdate: IUserAchievements = {
+              UserId: this.currentUser,
+              BadgeID: this.user_achievements.badgeID,
+              weeklyProgress: this.user_achievements.weeklyProgress,
+              dailyTracker: this.user_achievements.dailyTracker,
+              totalScore: this.user_achievements.totalScore,
+              lastActive: this.user_achievements.lastActive,
+              UnlockedAchievements: locked_and_unlocked[0],
+              LockedAchievements: locked_and_unlocked[1],
+              weeklytasks: this.user_achievements.weeklytasks + weeklytask,
+              tasksCompleted: this.user_achievements.tasksCompleted,
+              villainLevel: this.user_achievements.villainLevel
+            }
+
+            this.updateAchievements(this.currentUser, AchievemtUpdate);
+
             setTimeout(() => {
               this.showNotification = false;
             }, 5000);
@@ -189,7 +237,30 @@ export class TasksComponent implements OnInit {
 
 
   completeTask(task: IUserTasks): void {
-    // Implement complete task functionality here
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '250px',
+      data: { message: 'Have you completed this task?' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log(task.TaskId);
+        this.taskService.deleteTask(task.TaskId).subscribe(
+          (response) => {
+            console.log("Task successfully completed", response);
+            this.comicChoice = this.comicExpressions[Math.floor(Math.random() * 5)];
+            this.showImagePop = true;
+            setTimeout(() => {
+              this.showImagePop = false;
+            }, 2000);
+            this.handleProgressUpdate(task);
+          },
+          (error) => {
+            console.log("Error marking task as complete", error);
+          }
+        );
+      }
+    });
   }
 
   updateAchievements(currentUser: number, acheivement: IUserAchievements): void {
@@ -203,6 +274,83 @@ export class TasksComponent implements OnInit {
           console.log('error updating', error);
         }
       });
+  }
+
+  handleProgressUpdate(task: any) {
+    // update score and daily streak (using weigth * multiplier ? i guess this is how many days until due. reward for not precrastinating)
+    var multiplier_percentage = (new Date().getTime() - new Date(task.timeStamp).getTime()) / (new Date(task.dueDate).getTime() - new Date(task.timeStamp).getTime());
+    var addition_to_score = task.importance / multiplier_percentage;
+    var multiplier = 1;
+
+    if (addition_to_score >= 1000) {
+      multiplier = parseInt(addition_to_score.toString()[0]) + 1;
+    }
+    else if (addition_to_score < 0.0 && addition_to_score > -1000) {
+      multiplier = -1
+    }
+    else if (addition_to_score <= -1000) {
+      multiplier = parseInt(addition_to_score.toString()[0] + addition_to_score.toString()[1]) - 1;
+    }
+
+    // if the percentage is negative, reset streak to 0, else, increment. 
+    if (multiplier_percentage < 0.0) {
+      this.user_achievements.dailyTracker = 0;
+      //add something to put a notification for + whatever points, or minus whatever points. 
+    }
+    else {
+      this.user_achievements.dailyTracker += 1;
+    }
+
+    this.ProgressCount = this.user_achievements.weeklyProgress;
+
+    if (this.hasSundaySinceDate(new Date(this.user_achievements.lastActive))) {
+      this.ProgressCount = 0;
+    }
+
+    var add_to_progress_count = 0;
+
+    //this is where the error is, also score goes above 100 and tht shit fucks up with styling. fix that. 
+    if (this.IsDueBeforeSunday(new Date(task.dueDate))) {
+      add_to_progress_count = 1;
+    }
+
+    var locked_and_unlocked = this.achievements.determineAcheivements(
+      this.user_achievements.unlockedAchievements,
+      this.user_achievements.lockedAchievements,
+      this.user_achievements.dailyTracker,
+      "complete task",
+      this.user_achievements.tasksCompleted
+    );
+
+    this.totalScore = this.user_achievements.totalScore + (task.importance * multiplier);
+    this.userdetails.score = this.totalScore;
+
+    // update score and daily streak -- call to acheivement service. 
+    var AchievemtUpdate: IUserAchievements = {
+      UserId: this.currentUser,
+      BadgeID: this.user_achievements.badgeID,
+      weeklyProgress: this.ProgressCount + add_to_progress_count,
+      dailyTracker: this.user_achievements.dailyTracker,
+      totalScore: this.user_achievements.totalScore + (task.importance * multiplier),
+      lastActive: new Date().toDateString(),
+      UnlockedAchievements: locked_and_unlocked[0],
+      LockedAchievements: locked_and_unlocked[1],
+      weeklytasks: this.user_achievements.weeklytasks,
+      tasksCompleted: this.user_achievements.tasksCompleted + 1,
+      villainLevel: this.user_achievements.villainLevel
+    }
+    this.updateAchievements(this.currentUser, AchievemtUpdate);
+
+    //updating the users score so it can be shown in the social page/profile. 
+    this.userService.put(this.userdetails).subscribe({
+      error: (error) => {
+        console.error('An error occurred:', error);
+      },
+      complete: () => {
+        console.log("good")
+        this.refreshPage();
+      }
+    })
   }
 
   calculateSunday(due: string): boolean {
@@ -238,7 +386,44 @@ export class TasksComponent implements OnInit {
         error: (error) => {
           console.error('An error occurred:', error);
         },
-        complete: () => { }
+        complete: () => {
+          this.totalScore = this.user_achievements.totalScore;
+          //if there has been a sunday since last active, then determine new progress based on the stuff due. 
+          if (this.hasSundaySinceDate(this.user_achievements.lastActive)) {
+
+
+            //************************************* */
+            //first determine the villains fate here
+
+
+
+            //then change the villain to the next villain
+            //************************************* */
+            var taskCount = this.calculateProgress(); 
+            this.Achievements.update(this.currentUser,
+              {
+                UserId: this.currentUser,
+                BadgeID: this.user_achievements.badgeID,
+                weeklyProgress: this.user_achievements.weeklyProgress,
+                dailyTracker: this.user_achievements.dailyTracker,
+                totalScore: this.user_achievements.totalScore,
+                lastActive: new Date().toDateString(),
+                UnlockedAchievements: this.user_achievements.unlockedAchievements,
+                LockedAchievements: this.user_achievements.lockedAchievements,
+                weeklytasks: taskCount,
+                tasksCompleted: this.user_achievements.tasksCompleted,
+                villainLevel: this.user_achievements.villainLevel
+              })
+              .subscribe({
+                next: () => {
+                }
+                ,
+                error: error => {
+                  console.log('error updating', error);
+                }
+              });
+          }
+        }
       });
   }
 
@@ -274,5 +459,32 @@ export class TasksComponent implements OnInit {
         return dueDateA - dueDateB;
       });
     }
+  }
+
+  calculateProgress(): number {
+
+    // getting the date value of 'next sunday'
+    const today = new Date();
+    const daysOfWeek = today.getDay();
+    const daysUntilSunday = daysOfWeek === 0 ? 7 : 7 - daysOfWeek;
+    const nextSunday = new Date(today);
+    nextSunday.setDate(today.getDate() + daysUntilSunday);
+    nextSunday.setHours(0, 0, 0, 0);
+
+    // filtering the task list for only values between now and then 
+    var temp_tasks = this.tasks.filter(task => new Date(task.DueDate) <= nextSunday);
+
+    //returning the length of the filtered tasks... all that for that. has to be easier way 
+    return temp_tasks.length;
+  }
+
+  IsDueBeforeSunday(Due: Date): boolean {
+    const today = new Date();
+    const daysOfWeek = today.getDay();
+    const daysUntilSunday = daysOfWeek === 0 ? 7 : 7 - daysOfWeek;
+    const nextSunday = new Date(today);
+    nextSunday.setDate(today.getDate() + daysUntilSunday);
+    nextSunday.setHours(0, 0, 0, 0);
+    return Due <= nextSunday;
   }
 }
